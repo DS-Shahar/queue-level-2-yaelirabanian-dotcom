36b4d29f2ced9379cf2c918c094c7811edb4f180
yaelirabanian-dotcom <yaeli.rabanian@gmail.com>
2026-01-03 21:38:28 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,85 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+public class Main {
+    public static void main(String[] args) {
+    }
+  
+    public static boolean isIn(Queue<Integer> q1, int x) {
+        Queue<Integer> q2 = new Queue<>();
+        boolean f = false;
+        while (!q1.isEmpty()) {
+            int y = q1.remove();
+            if (y == x)
+                f = true;
+            q2.insert(y);
+        }
+        while (!q2.isEmpty())
+            q1.insert(q2.remove());
+        return f;
+    }
+
+  
+    public static Queue <Integer> ex1(Queue <Integer> q1) {
+        Queue <Integer> q2 = new Queue<>();
+        Queue <Integer> q3 = new Queue<>();
+        int pr = q1.remove();
+        q3.insert(pr);
+        int count = 1;
+        while (!q1.isEmpty()) {
+            int cr = q1.remove();
+            q3.insert(cr);
+            if (cr == pr)
+                count++;
+            else {
+                q2.insert(count);
+                count = 1;
+                pr = cr;
+            }
+        }
+        q2.insert(count);
+        while (!q3.isEmpty())
+            q1.insert(q3.remove());
+        return q2;
+    }
+  
+    public static Queue<Integer> ex5(Queue <Integer> q1, Queue <Integer> q2) {
+        Queue <Integer> q3 = new Queue<>();
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            if (q1.head() <= q2.head())
+                q3.insert(q1.remove());
+            else
+                q3.insert(q2.remove());
+        }
+        while (!q1.isEmpty())
+            q3.insert(q1.remove());
+        while (!q2.isEmpty())
+            q3.insert(q2.remove());
+        return q3;
+    }
+
+  
+    public static int ex6(Queue <Integer> q1) {
+        Queue <Integer> q2 = new Queue<>();
+        int cr = 0;
+        int crSum = 0;
+        int max = 0;
+        int maxSum = 0;
+        while (!q1.isEmpty()) {
+            int x = q1.remove();
+            q2.insert(x);
+            if (x % 2 == 0) {
+                cr++;
+                crSum += x;
+                if (cr > max) {
+                    max = cr;
+                    maxSum = crSum;
+                }
+            }
+            else {
+                cr = 0;
+                crSum = 0;
+            }
+        }
+        while (!q2.isEmpty())
+            q1.insert(q2.remove());
+        return maxSum;
+    }
 }
